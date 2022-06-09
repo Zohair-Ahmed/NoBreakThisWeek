09c63286d5d9633ca8ce284177a626f415830759
Zohair Ahmed <Zohair@Zohairs-MBP.hitronhub.home>
2022-06-09 10:26:03 -0400

    :art: (#24) - Retreive and compare web scraped data with MongoDB data
    The basic functionality is complete. The Python script uses the data from MongoDB
    and the web scraped data to determine the change in status of the One Piece chapter.

--- python/NoBreakThisWeek.py
+++ python/NoBreakThisWeek.py
@@ -9,6 +9,7 @@ load_dotenv(find_dotenv())
 
 opTitle = "One Piece"
 baseUrl = "https://onepiecechapters.com"
+collectionID = 0
 
 MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
 CLUSTER_NAME = os.environ.get('CLUSTER_NAME')
@@ -39,10 +40,10 @@ def getLatestChapterLink(opLatestCh):
     return baseUrl + opLatestCh['href']
 
 
-def dataToJSON(opStatus, opLatestNumber, opLatestTitle, opLatestChLink):
+def postOPInfo(opStatus, opLatestNumber, opLatestTitle, opLatestChLink):
     """Getting the data collected to parse to JSON, which is sent to MongoDB"""
     webData = {
-        "_id" : 0,
+        "_id" : collectionID,
         "chapter-num" : opLatestNumber,
         "title" : opLatestTitle,
         "status" : opStatus,
@@ -52,28 +53,66 @@ def dataToJSON(opStatus, opLatestNumber, opLatestTitle, opLatestChLink):
     return webData
 
 
+def getOPInfo():
+    """Return the current data stored for the One Piece chapter"""
+    getInfo = collection.find_one({"_id": collectionID})
+    return getInfo
+
+
+def updateOPInfo_translating(opStatus):
+    """Update the One Piece info to now say the a chapter is being translated"""
+    statusUpdate = { "$set": {"status": opStatus} }
+    collection.update_one({"_id": collectionID}, statusUpdate)
+
+
+def updateOPInfo_released(opLatestNumber, opLatestTitle, opStatus, opLatestChLink):
+    """Update the chapter number, title, and link to match just release chapter"""
+    replaceInfo = {
+        "chapter-num" : opLatestNumber,
+        "title" : opLatestTitle,
+        "status" : opStatus,
+        "link" : opLatestChLink
+    }
+
+    collection.replace_one({"_id" : collectionID}, replaceInfo)
+
 # get status specific information
 getStatusUrl = baseUrl + "/projects"
 statusRequest = requests.get(getStatusUrl)
 htmlStatusDoc = BeautifulSoup(statusRequest.text, "html.parser")
-opStatusCard = htmlStatusDoc.find_all(text="One Piece")[0].parent.parent
+web_opStatusCard = htmlStatusDoc.find_all(text="One Piece")[0].parent.parent
 
 # get chapter specific information
 getChapterUrl = baseUrl + "/mangas/5/one-piece"
 chapterRequest = requests.get(getChapterUrl)
 htmlChapterDoc = BeautifulSoup(chapterRequest.text, "html.parser")
-opLatestCh = htmlChapterDoc.find(class_="col-span-2").find("a")
+web_opLatestCh = htmlChapterDoc.find(class_="col-span-2").find("a")
 
 # quit program if website is not reached
 if statusRequest.status_code != 200 or chapterRequest.status_code != 200:
     quit()
 
-# organize chapter specific info to variables
-opStatus = getChapterStatus(opStatusCard)
-opLatestNumber = getLatestChapterNumber(opLatestCh)
-opLatestTitle = getLatestChapterTitle(opLatestCh)
-opLatestChLink = getLatestChapterLink(opLatestCh)
+# organize web scraped chapter specific info to variables
+web_opStatus = getChapterStatus(web_opStatusCard)
+web_opLatestNumber = getLatestChapterNumber(web_opLatestCh)
+web_opLatestTitle = getLatestChapterTitle(web_opLatestCh)
+web_opLatestChLink = getLatestChapterLink(web_opLatestCh)
 
-# insert to database
-onePieceData = dataToJSON(opStatus, opLatestNumber, opLatestTitle, opLatestChLink)
-collection.insert_one(onePieceData)
+# organize database scraped chapter specific info to variables
+db_latestOpInfo = getOPInfo()
+db_opStatus = db_latestOpInfo["status"]
+db_opLatestNumber = db_latestOpInfo["chapter-num"]
+db_opLatestTitle = db_latestOpInfo["title"]
+db_opLatestChLink = db_latestOpInfo["link"]
+
+# there has been a status update
+if web_opStatus != db_opStatus:
+    # if there are been no update to chapter number, it is translating, else the chapter has been released
+    if web_opLatestNumber == db_opLatestNumber:
+        print(f"The next chapter is now being translated!")
+        updateOPInfo_translating(web_opStatus)
+    else:
+        print(f"{web_opLatestNumber} is now released!")
+        updateOPInfo_released(web_opLatestNumber, web_opLatestTitle, web_opStatus, web_opLatestChLink)
+else:
+    quit()
